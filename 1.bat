7879aa3b99bade6a4880daa6a7404ea9deb9878c
FFFFDDDD25 <phone993825@gmail.com>
2020-11-23 17:58:06 +0800

    foo

--- 1.bat
+++ 1.bat
@@ -4,14 +4,8 @@
 	git clean
 	git commit -m "foo"
 	git push  -u origin master 
+	git pull
+	docker build  . -t test004
 	rem dotnet build
 	rem rem 是註解
-	rem rem
-	rem rem
-	rem rem
-	rem rem
-	rem rem
-	rem rem
-	rem rem
-	rem rem
 	rem rem
